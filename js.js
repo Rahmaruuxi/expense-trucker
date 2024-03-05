07eb5305cd384faf5113d31a6d97912d5ad65b4a
Rahmaruuxi <rahmaruun442@gmail.com>
2024-03-05 08:50:04 +0300

    expense trucker

--- js.js
+++ js.js
@@ -1,29 +1,3 @@
-// // Get the modal element
-// const modal = document.getElementById("expenseModal");
-
-// // Get the button that opens the modal
-// const openModalBtn = document.getElementById("openModalBtn");
-
-// // Get the close button (span) inside the modal
-// const closeBtn = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal
-// openModalBtn.onclick = function () {
-//   modal.style.display = "block";
-// };
-
-// // When the user clicks the close button, close the modal
-// closeBtn.onclick = function () {
-//   modal.style.display = "none";
-// };
-
-// // When the user clicks anywhere outside the modal, close it
-// window.onclick = function (event) {
-//   if (event.target === modal) {
-//     modal.style.display = "none";
-//   }
-// };
-
 function openPopup() {
   popup.classList.add("open-popup");
 }
